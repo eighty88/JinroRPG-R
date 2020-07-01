dad82f72454b110f256abe855e6033f2e6d14a75
eighty88 <49605393+eighty88@users.noreply.github.com>
2020-07-01 18:47:00 +0900

    Update JinroChatCommand.java

--- src/main/java/com/github/eighty88/jinrorpg/command/JinroChatCommand.java
+++ src/main/java/com/github/eighty88/jinrorpg/command/JinroChatCommand.java
@@ -1,5 +1,7 @@
 package com.github.eighty88.jinrorpg.command;
 
+import com.github.eighty88.jinrorpg.api.LunaChatTranslateAPI;
+import com.github.eighty88.jinrorpg.api.TranslateType;
 import com.github.eighty88.jinrorpg.controller.GameController;
 import com.github.eighty88.jinrorpg.player.JinroPlayer;
 import org.bukkit.ChatColor;
@@ -12,7 +14,7 @@ public class JinroChatCommand {
             JinroPlayer player = JinroPlayer.getJinroPlayer((Player) sender);
             if(player.isJinro()) {
                 for(JinroPlayer jinro: GameController.Jinro) {
-                    jinro.getPlayer().sendMessage(ChatColor.RED + "[人狼チャット]" + player.getName() + ": " + args);
+                    jinro.getPlayer().sendMessage(ChatColor.RED + "[人狼チャット]" + player.getName() + ": " + args + ChatColor.GOLD + " (" + LunaChatTranslateAPI.Translate(args, TranslateType.GOOGLE_IME) + ")");
                 }
             }
             return true;
